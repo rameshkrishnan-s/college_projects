c277a0fd06b55e1873e6ee176f09476a3f7a07e2
rameshkrishnan-s <sundarrajanrameshkrishnan@gmail.com>
2024-12-09 07:52:21 +0530

    Created ToDo list component

---
+++ to-do-list/src/components/TodoItems.jsx
@@ -0,0 +1,18 @@
+import tick from "../assets/tick.png"
+import not_tick from "../assets/not_tick.png"
+import delete_icon from "../assets/delete.png"
+const TodoItems = ({text}) => {
+  return (
+    <div className="flex items-center my-3 gap-2">
+
+        <div className="flex flex-1 items-center cursor-pointer">
+          <img src={tick} alt="" className="w-7"/>
+          <p className="text-slate-700 ml-4 text-[17px]">{text}</p>
+        </div>
+
+        <img src={delete_icon} alt="" className="w-4 cursor-pointer"/>
+    </div>
+  )
+}
+
+export default TodoItems
--- to-do-list/src/pages/Todo.jsx
+++ to-do-list/src/pages/Todo.jsx
@@ -1,4 +1,5 @@
 import todo_icon from "../assets/todo_icon.png";
+import TodoItems from "../components/TodoItems";
 
 const Todo = () => {
 
@@ -15,9 +16,16 @@ return(
 
         <div className="flex items-center my-7 bg-gray-200 rounded-full">
             <input className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600" type="text" placeholder="Add your task" />
-            <button className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer">Add +</button>
+            <button className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer">
+                Add +
+            </button>
         </div>
 
+            <TodoItems text="Learn react"/>
+
+        <div>
+
+        </div>
 
     </div>
 )
